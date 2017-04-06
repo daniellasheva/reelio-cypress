3a409d98cd19d2613b4f6037af667fecb1be52cd
daniellasheva <daniellasheva@gmail.com>
2017-04-06 14:40:43 -0400

    login

---
+++ cypress/integration/app_spec.js
@@ -0,0 +1,42 @@
+describe('Reelio Staging', function(){
+
+	it('should visit site', function(){
+		cy.visit('3.1-staging.reelio.com')
+		.get('#app')
+		.get('h1')
+		.get('svg')
+		.should('contain', 'Reelio')
+	})
+
+	it.only('Should log in as AM: brittany@reeliolabs.com', function(){
+		cy.visit('3.1-staging.reelio.com')
+		cy.get('#app')
+		.get('form').find('input').first()
+		.type('brittany@reeliolabs.com')
+		cy.get('form').find('input').last()
+		.type('password')
+		.get('button').last().click()
+		cy.wait(7000)
+		// cy.server({
+		// 	delay: 1000,
+		// 	method: 'GET',
+		// 	response: {}
+		// })
+		// .route(/campaigns/, "fixtures:campaigns.json")
+		cy.get('button').contains('New Campaign').click()
+	})
+
+	it('Should click Create New Campaign', function(){
+		cy.get('button').contains('New Campaign').click()
+	})
+
+
+	it('Should click Create New Campaign', function(){
+		cy.visit('3.1-staging.reelio.com/campaigns')
+		cy.server().route("GET", /campaigns/, "fixtures:campaigns.json")
+		cy.get('button').contains('New Campaign').click()
+		// cy.wait('/campaigns')
+		//cy.get('button').contains('New Campaign')
+		//.click()
+	})
+})
