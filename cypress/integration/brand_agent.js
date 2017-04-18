89e75201d9626d9001f00198466e668c4048d19c
gabriel <gabriel@reeliolabs.com>
2017-04-18 17:15:26 -0400

    more brand agent

--- cypress/integration/brand_agent.js
+++ cypress/integration/brand_agent.js
@@ -1,5 +1,11 @@
+/*
+Brand agent critical path
+*/
+
 describe('Brand agent', function (){
   it('should login as brand agent', function(){
+
+// run the site locally
     cy.visit('http://localhost:8080')
     cy.title().should('include', 'Login')
     cy
@@ -15,26 +21,61 @@ describe('Brand agent', function (){
       .click()
     cy.title().should('include', 'Campaigns')
 
+// stubbing these three with blank responses
     cy
       .server()
-      .route({
-        method: "GET",
-        url: "https://staging.reelio.com/api/v3/notifications/?limit=10",
-        response: "fixture:enterprises_me"
-      })
+      .route("GET", "https://staging.reelio.com/api/v3/notifications/?limit=10", {})
     cy
       .server()
-      .route({
-        method: "GET",
-        url: "https://staging.reelio.com/api/v3/resources/status/?limit=10",
-        response: "fixture:resources_status"
-      })
+      .route("GET", "https://staging.reelio.com/api/v3/resources/status/?limit=10", {})
+    cy
+      .server()
+      .route("GET", "https://staging.reelio.com/api/v3/campaigns/?&ordering=-latest_event_dtime&limit=10", {})
   })
 
-  it.skip('should navigate to brands list', function(){
+  it('should navigate to brands list', function(){
     cy
       .get("button").contains("Brands")
       .as("Brands")
+      .click()
+
+    cy.title().should('include', 'Brands')
+  })
+
+  it('should click Create New', function(){
+    cy
+      .get("button").contains("Create New")
+      .click()
+
+// stubbing verticals and keywords with actual data
+// this isn't working, though
+
+    cy
+      .server()
+      .route({
+        method: "GET",
+        url: "https://staging.reelio.com/api/v3/annotations/?category=verticals&limit=10",
+        response: "fixture:category_verticals"
+      })
+    cy
+      .server()
+      .route({
+        method: "GET",
+        url: "https://staging.reelio.com/api/v3/annotations/?category=keywords&limit=10",
+        response: "fixture:category_keywords"
+      })
+  })
+
+  it('should fill in Create Brand form', function(){
+    cy
+      .get("input[maxlength='50']")
+      .type("brand name")
+    cy
+      .get("textarea")
+      .type("about")
+    cy
+      .get("div[data-react-toolbox='check']")
+      .first()
       .click()
   })
 
