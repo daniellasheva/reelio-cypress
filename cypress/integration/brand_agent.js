46ac926b574e726b0b3542865bd7c8f76dc4da44
gabriel <gabriel@reeliolabs.com>
2017-04-19 17:14:14 -0400

    stuck on 401 errors within cypress

--- cypress/integration/brand_agent.js
+++ cypress/integration/brand_agent.js
@@ -3,8 +3,58 @@ Brand agent critical path
 */
 
 describe('Brand agent', function (){
-  it('should login as brand agent', function(){
 
+  beforeEach(function (){
+// stubbing these
+    cy
+      .server()
+      .route("GET", "https://staging.reelio.com/api/v3/notifications/?limit=10", {})
+    cy
+      .server()
+      .route("GET", "https://staging.reelio.com/api/v3/campaigns/?&ordering=-latest_event_dtime&limit=10", {})
+    cy
+      .server()
+      .route({
+        method: "GET",
+        url: "https://staging.reelio.com/api/v3/brands/?ordering=-latest_event_dtime&limit=10",
+        response: "fixture:brands",
+        status: 200
+      })
+    cy
+      .server()
+      .route({
+        method: "GET",
+        url: "https://staging.reelio.com/api/v3/enterprises/me/?limit=10",
+        response: "fixture:enterprises_me",
+        status: 200
+      })
+    cy
+      .server()
+      .route({
+        method: "GET",
+        url: "https://staging.reelio.com/api/v3/resources/status/?limit=10",
+        response: "fixture:resources_status",
+        status: 200
+      })
+    cy
+      .server()
+      .route({
+        method: "GET",
+        url: "https://staging.reelio.com/api/v3/annotations/?category=verticals&limit=10",
+        response: "fixture:category_verticals",
+        status: 200
+      })
+    cy
+      .server()
+      .route({
+        method: "GET",
+        url: "https://staging.reelio.com/api/v3/annotations/?category=keywords&limit=10",
+        response: "fixture:category_keywords",
+        status: 200
+      })
+  })
+
+  it('should login as brand agent', function(){
 // run the site locally
     cy.visit('http://localhost:8080')
     cy.title().should('include', 'Login')
@@ -20,17 +70,6 @@ describe('Brand agent', function (){
       .get("button[type='submit']")
       .click()
     cy.title().should('include', 'Campaigns')
-
-// stubbing these three with blank responses
-    cy
-      .server()
-      .route("GET", "https://staging.reelio.com/api/v3/notifications/?limit=10", {})
-    cy
-      .server()
-      .route("GET", "https://staging.reelio.com/api/v3/resources/status/?limit=10", {})
-    cy
-      .server()
-      .route("GET", "https://staging.reelio.com/api/v3/campaigns/?&ordering=-latest_event_dtime&limit=10", {})
   })
 
   it('should navigate to brands list', function(){
@@ -38,6 +77,8 @@ describe('Brand agent', function (){
       .get("button").contains("Brands")
       .as("Brands")
       .click()
+    cy
+      .request("POST", "https://staging.reelio.com/api/v3/auth/login/", {email: "reeliotestagent@gmail.com", password: "password"})
 
     cy.title().should('include', 'Brands')
   })
@@ -46,24 +87,6 @@ describe('Brand agent', function (){
     cy
       .get("button").contains("Create New")
       .click()
-
-// stubbing verticals and keywords with actual data
-// this isn't working, though
-
-    cy
-      .server()
-      .route({
-        method: "GET",
-        url: "https://staging.reelio.com/api/v3/annotations/?category=verticals&limit=10",
-        response: "fixture:category_verticals"
-      })
-    cy
-      .server()
-      .route({
-        method: "GET",
-        url: "https://staging.reelio.com/api/v3/annotations/?category=keywords&limit=10",
-        response: "fixture:category_keywords"
-      })
   })
 
   it('should fill in Create Brand form', function(){
