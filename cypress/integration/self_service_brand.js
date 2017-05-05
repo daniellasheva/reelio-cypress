e5882df84abac08c4f38c73dad4453dd304703e8
daniellasheva <daniellasheva@gmail.com>
2017-05-05 16:04:02 -0400

    campaign creation complete

--- cypress/integration/self_service_brand.js
+++ cypress/integration/self_service_brand.js
@@ -1,4 +1,8 @@
-describe('Self Service Brand', function (){
+//TODO: figure out why campaign button doesn't show up the first time (/me doesn't work)
+//TODO: change the campaign name every time
+
+
+describe('Searchligh Brand', function (){
   var token
   beforeEach(function (){
 // if you have a jwt, get it and save it
@@ -15,7 +19,7 @@ describe('Self Service Brand', function (){
     cy
       .get("input[type='email']")
       .as("email")
-      .type("dafu@reeliolabs.com")
+      .type("reeliotestselfservice@gmail.com")
     cy
       .get("input[type='password']")
       .as("password")
@@ -32,21 +36,29 @@ describe('Self Service Brand', function (){
 
   it('should click New Campaign and select brand', function(){
     cy
-      .get("button", {timeout: 6000}).contains("New Campaign", ).click()
+	  .wait(8000)
+      .get("button").contains("New Campaign").click()
 	  .get(".styles__listText___251Z7").click()
   })
 
   it('should fill out campaign form', function(){
     cy
-	  .get('div').contains("Da Fu's Campaign").parent().find("input").type(2)
-	  .get("div").contains("Most Important Campaign Goal").parent().find("input").click()
-	//select verticals
+	  //wait for campaign title to populate
+	  .wait(3000)
 	cy
+	//Click on Campaign title
+	  .get('.theme__inputElement___27dyY').first().click()
+	  .type("{selectall}{backspace}")
+	  .type("new campaign")
+	  .get("div").contains("Most Important Campaign Goal").parent().find("input").click()
+	cy
+	//Select Verticals
 	  .get("li").contains("Views").click().click()
 	  .get("div").contains("Auto/Vehicle").parent().find("label").click()
 	  .get("div").contains("Education").parent().find("label").click()
 	  .get("div").contains("Fitness/Health").parent().find("label").click()
 	 cy
+	 //
 	   .get("div").contains("Describe your opportunity").parent().find("textarea")
 	   .type("One you won't want to miss")
 	   .get("div").contains("Describe your product").parent().find("textarea")
@@ -54,27 +66,38 @@ describe('Self Service Brand', function (){
 	 cy
 	   .get("div").contains("Enter your budget").parent().find("input")
 	   .type(4300)
-	   .get("button").contains("Submit").click()
+	   .get("button").parent().contains("Publish").click({force:true})
    })
 
-   it.skip('Should upload Campaign Banner', function(){
-	   cy
-	   	.get('button').contains('UPLOAD IMAGE').click()
-	    //.fixture('images/frivolous_monkey.jpg')
 
-   })
+/* Marketplace Only Tests
 
-   it.skip('Should choose an application deadline', function(){
-	 cy
-	   .get("div").contains("Application Deadline").parent().find("input").click()
-	   .get(".theme__month___27O28").children()
-	   .get(".theme__days___3kAIy").children()
-	   .get("span").contains(9).click()
-	   .get("button").contains("Ok").click()
-  })
+//Banner File Upload.
 
-  it('Should publish the campaign', function(){
+   // it('Should upload Campaign Banner', function(){
+   //  cy
+   //  	.get('button').contains('UPLOAD IMAGE').click()
+   //   .fixture('images/frivolous_monkey.jpg').as('logo')
+   // get('input[type=file]').then(function($input){
+   // return Cypress.Blob.base64StringToBlob(this.logo, "image/jpg").then(function(blob){
+   // 	$input.fileupload("add", {files: blob})
+   // })
+   //    })
+   // })
 
-  })
+//Application Deadline
+  //  it.skip('Should choose an application deadline', function(){
+  // cy
+  //   .get("div").contains("Application Deadline").parent().find("input").click()
+  //   .get(".theme__month___27O28").children()
+  //   .get(".theme__days___3kAIy").children()
+  //   .get("span").contains(9).click()
+  //   .get("button").contains("Ok").click()
+  // })
+
+  // it('Should publish the campaign', function(){
+  //
+  // })
+*/
 
 })
